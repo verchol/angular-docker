73b3fd1dc34f1b47cbf6b24a310087a32dd5bc42
Oleg verhovsky <verchol@gmail.com>
2016-04-25 17:51:50 +0300

    fix selenium adress

--- protractor-js.conf.js
+++ protractor-js.conf.js
@@ -1,10 +1,14 @@
 var data = module.exports = require('./protractor-shared.js');
 var config = data.config;
 
-config.baseUrl = 'http://localhost:8001/';
+
+config.baseUrl = process.env.SELENIUM || 'http://selenium:4444/wd/hub';
+console.log('we are bout to connect to selenium server ' + config.baseUrl);
+
 // TODO: remove exclusion when JS verison of scrolling benchmark is available
 config.exclude.push('dist/js/cjs/benchmarks_external/e2e_test/naive_infinite_scroll_spec.js');
 config.exclude.push('dist/js/cjs/benchmarks_external/e2e_test/naive_infinite_scroll_perf.js');
 
-data.createBenchpressRunner({ lang: 'js' });
-
+data.createBenchpressRunner({
+  lang: 'js'
+});
